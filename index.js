e8752c30d112a8adffb4e273a41d90cbfd96f8ba
Carlos <carlosoescalante1@gmail.com>
2022-07-20 22:14:37 -0300

    se sube la página inicial

---
+++ index.js
@@ -0,0 +1,311 @@
+let PELICULAS = [
+
+    {
+    titulo:"Cadena Perpetua",
+    descripcion:"Andy Dufresne es encarcelado por matar a su esposa y al amante de esta. Tras una dura adaptación, intenta mejorar las condiciones de la prisión y dar esperanza a sus compañeros.",
+    año:1994,
+    género:"Drama",
+    score:9.3,
+    reparto:"Tim Robbins, Morgan Freeman, Bob Gunton",
+    imagen:"https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",
+
+    },
+
+    {
+        titulo:"El Padrino",
+        descripcion: "El envejecido patriarca de una dinastía del crimen organizado en la ciudad de Nueva York de la posguerra transfiere el control de su imperio clandestino a su reacio hijo menor.",
+        año:1972,
+        género:"Crimen, Drama",
+        score:9.3,
+        reparto: "Marlon Brando, Al Pacino, James Caan",
+        imagen:"https://images-na.ssl-images-amazon.com/images/I/714ZOEiVNtL._RI_.jpg",
+    
+    },
+
+    {
+        titulo:"Batman: El caballero de la noche",
+        descripcion:"Cuando la amenaza conocida como el Joker causa estragos y el caos en Gotham City, Batman debe aceptar una de las mayores pruebas psicológicas y físicas para luchar contra la injusticia.",
+        año:2008,
+        género:"Acción, Crimen, Drama",
+        score:9.0,
+        reparto:"Christian Bale, Heath Ledger, Aaron Eckhart",
+        imagen:"https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg",
+    
+    },
+
+    {
+        titulo:"El Padrino Parte II",
+        descripcion:"Se retratan los inicios de la vida y la carrera de Vito Corleone en el Nueva York de los años 20, mientras su hijo, Michael, amplía y refuerza su control sobre el sindicato del crimen familiar.",
+        año:1974,
+        género:"Crimen, Drama",
+        score:9.0,
+        reparto: "Al Pacino, Robert De Niro, Robert Duvall",
+        imagen:"https://mx.web.img3.acsta.net/medias/nmedia/18/68/09/06/20197890.jpg",
+    
+    },
+
+    {
+        titulo:"12 hombres sin piedad",
+        descripcion:"Un miembro del jurado trata de evitar un error judicial obligando al resto del jurado a reconsiderar las pruebas.",
+        año:1957,
+        género:"Crimen, Drama",
+        score:9.0,
+        reparto:"Henry Fonda, Lee J. Cobb, Martin Balsam",
+        imagen:"https://pics.filmaffinity.com/Doce_hombres_en_pugna-223267259-large.jpg",
+    
+    },
+
+    {
+        titulo:"La lista de Schindler",
+        descripcion:"En la Polonia ocupada por los alemanes durante la Segunda Guerra Mundial, el industrial Oskar Schindler se preocupa por sus trabajadores judíos tras presenciar su persecución por los nazis.",
+        año:1993,
+        género:"Biografía, Drama, Historia",
+        score:9.0,
+        reparto:"Liam Neeson, Ralph Fiennes, Ben Kingsley",
+        imagen:"https://www.diariovivo.com/wp-content/uploads/2019/07/9788413140957.jpg",
+    
+    },
+
+    {
+        titulo:"El señor de los anillos: El retorno del rey",
+        descripcion:"Gandalf y Aragorn lideran el mundo de los hombres contra la armada de Sauron para distraer su atención de Frodo y Sam, quienes se aproximan al Monte del Destino con el Anillo Único.",
+        año:2003,
+        género:"Acción, Aventura, Drama",
+        score:9.0,
+        reparto:"Elijah Wood, Viggo Mortensen, Ian McKellen",
+        imagen:"https://pics.filmaffinity.com/El_se_or_de_los_anillos_El_retorno_del_rey-164432989-large.jpg",
+    
+    },
+
+    {
+        titulo:"Pulp Fiction",
+        descripcion:"Las vidas de dos mafiosos, un boxeador, la esposa de un gánster y un par de bandidos se entrelazan en cuatro historias de violencia y redención.",
+        año:1994,
+        género:"Crimen, Drama",
+        score:8.9,
+        reparto:"John Travolta, Uma Thurman, Samuel L. Jackson",
+        imagen:"https://pics.filmaffinity.com/Pulp_Fiction-210382116-large.jpg",
+    
+    },
+
+    {
+        titulo:"El señor de los anillos: La comunidad del anillo",
+        descripcion:"Un hobbit de la Comarca y ocho compañeros emprenden un viaje para destruir el poderoso Anillo Único y salvar la Tierra Media del Señor Oscuro Sauron.",
+        año:2001,
+        género:"Acción, Aventura, Drama",
+        score:8.8,
+        reparto:"Elijah Wood, Orlando Bloom, Ian McKellen",
+        imagen:"https://http2.mlstatic.com/D_NQ_NP_797659-MLM26842726841_022018-O.jpg",
+    
+    },
+
+    {
+        titulo:"El bueno, el feo y el malo",
+        descripcion:"Durante la Guerra de Secesión, tres cazarrecompensas se lanzan a la búsqueda de un tesoro que ninguno puede localizar sin la ayuda de los otros dos.",
+        año:1966,
+        género:"Aventura, Western",
+        score:8.8,
+        reparto:"Clint Eastwood, Eli Wallach, Lee Van Cleef",
+        imagen:"https://es.web.img3.acsta.net/pictures/14/03/21/13/49/293020.jpg",
+    
+    },
+
+    {
+        titulo:"Forrest Gump",
+        descripcion:"Las presidencias de Kennedy y Johnson, la guerra de Vietnam, el escándalo de Watergate y otros hechos históricos se desarrollan desde la perspectiva de un hombre de Alabama con un coeficiente intelectual de 75, cuyo único deseo es reunirse con su novia de la infancia.",
+        año:1994,
+        género:"Drama, Romance",
+        score:8.8,
+        reparto:"Tom Hanks, Robin Wrigth, Gary Sinise",
+        imagen:"https://pics.filmaffinity.com/Forrest_Gump-212765827-large.jpg",
+    
+    },
+
+    {
+        titulo:"El club de la pelea",
+        descripcion:"Un oficinista insomne y un desentendido fabricante de jabones forman un club de lucha clandestino que se convierte en mucho más.",
+        año:1999,
+        género:"Drama",
+        score:8.8,
+        reparto:"Brad Pitt, Edward Norton, Meat Loaf",
+        imagen:"https://elegirhoy.com/uploads/fichas-eventos-imagenes/el-club-de-la-lucha-portada.jpg",
+    
+    },
+
+    {
+        titulo:"El origen",
+        descripcion:"A un ladrón que roba secretos corporativos a través del uso de la tecnología de compartir sueños, se le da la tarea de implantar una idea en la mente de un jefe de una gran empresa.",
+        año:2010,
+        género:"Acción, Aventura, Ciencia Ficción",
+        score:8.8,
+        reparto:"Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page",
+        imagen:"https://cdn.culturagenial.com/es/imagenes/inception-cke.jpg",
+    
+    },
+
+    {
+        titulo:"El señor de los anillos: Las dos torres",
+        descripcion:"Mientras Frodo y Sam se acercan a Mordor con la ayuda del astuto Gollum, la comunidad dividida se enfrenta al nuevo aliado de Sauron, Saruman, y a sus hordas de Isengard.",
+        año:202,
+        género:"Acción, Aventura, Drama",
+        score:8.8,
+        reparto:"Elijah Wood, Viggo Mortensen, Ian McKellen",
+        imagen:"https://image.tmdb.org/t/p/w500/qHEU9067PL8iIZULg7q58NvoWlS.jpg",
+    
+    },
+
+    {
+        titulo: "El Imperio contraataca",
+        descripcion:"Después de que los Rebeldes son brutalmente vencidos por el Imperio en el planeta de hielo Hoth, Luke Skywalker comienza su entrenamiento Jedi con Yoda, mientras sus amigos son perseguidos por toda la galaxia por Darth Vader y el cazarrecompensas Boba Fett.",
+        año:1980,
+        género:"Acción, Aventura, Fantasía",
+        score:8.7,
+        reparto:"Mark Hamill, Harrison Ford, Carrie Fisher",
+        imagen:"https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2016/11/guerra-galaxias-episodio-v-imperio-contraataca.jpg?itok=VAWPOKpp",
+    
+    },
+
+    {
+        titulo:"Matrix",
+        descripcion:"Cuando una bella desconocida lleva al hacker Neo a un inframundo prohibido, descubre la impactante verdad: la vida que conoce es un elaborado engaño de una ciberinteligencia malvada.",
+        año:1999,
+        género:"Acción, Ciencia Ficción",
+        score:8.7,
+        reparto:"Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
+        imagen:"https://cdn.culturagenial.com/es/imagenes/cartel-matrix.jpg",
+    
+    },
+
+    {
+        titulo:"Buenos Muchachos",
+        descripcion:"La historia de Henry Hill y su vida en la mafia, abarcando su relación con su esposa Karen Hill y sus socios mafiosos Jimmy Conway y Tommy DeVito en el sindicato del crimen italoamericano.",
+        año:1990,
+        género:"Biografía, Crimen, Drama",
+        score:8.7,
+        reparto:"Robert De Niro, Ray Liotta, Joe Pesci",
+        imagen:"https://pics.filmaffinity.com/Uno_de_los_nuestros-722948658-large.jpg",
+    
+    },
+
+    {
+        titulo:"Alguien voló sobre el nido del cuco",
+        descripcion:"Un criminal alega locura para ser enviado a un centro de salud mental, donde se rebela contra la opresiva enfermera y moviliza a los pacientes, a quienes tienen atemorizados.",
+        año:1975,
+        género:"Drama",
+        score:8.7,
+        reparto:"Jack Nicholson, Louise Fletcher, Michael Berryman",
+        imagen:"https://play-lh.googleusercontent.com/kTqHFStOG0OghpKZ19Wpo18jjwFwED8GMSVGVv_6Vs0eeAyMTBfsWeCb2pyZanFEn7L_",
+    
+    },
+
+    {
+        titulo:"Seven",
+        descripcion:"Dos detectives, un novato y un veterano, dan caza a un asesino en serie cuyo móvil son los siete pecados capitales.",
+        año: 1995,
+        género:"Crimen, Drama, Misterio",
+        score:8.6,
+        reparto: "Morgan Freeman, Brad Pitt, Kevin Spacey",
+        imagen:"https://pics.filmaffinity.com/Seven_Se7en-734875211-large.jpg",
+    
+    },
+
+    {
+        titulo:"Los siete samuráis",
+        descripcion:"Un pueblo pobre atacado por bandidos recluta a siete samuráis desempleados para que les ayuden a defenderse.",
+        año:1954,
+        género:"Acción, Drama",
+        score:8.6,
+        reparto:"Toshiro Mifune, Takashi Shimura, Keiko Tsushima",
+        imagen:"https://www.encadenados.org/rdc/images/stories/todo_demas/num_102/siete-samurais-0.jpg",
+    
+    },
+
+
+
+
+];
+
+
+const contenedor = document.getElementById("container");
+const tarjeta = document.getElementById("cards-home");
+
+
+
+
+
+
+const rendercards = () =>{
+
+    let cont = 0;
+
+    PELICULAS.forEach(pelicula => {
+
+        
+        cont= cont + 1;
+
+        const card = document.createElement('div');
+        card.classList.add('card-home');
+
+
+
+
+        const contenedortarjeta = document.createElement('div');
+        contenedortarjeta.classList.add('contentcard-home');
+        const imagentarjeta = document.createElement('div');
+        imagentarjeta.classList.add('imgcard-home');
+
+        const puestopelicula = document.createElement('h6');
+        const renderpuestopelicula = document.createTextNode("Puesto N°: " + cont);
+        puestopelicula.appendChild(renderpuestopelicula);
+        contenedortarjeta.appendChild(puestopelicula);
+
+
+        const imgcard = document.createElement('img');
+        imgcard.setAttribute('src',pelicula.imagen);
+        imgcard.setAttribute('alt',pelicula.titulo);
+        imgcard.classList.add("img-home");
+        imagentarjeta.appendChild(imgcard);
+
+        const titulopelicula = document.createElement('h5');
+        const rendertitulopelicula = document.createTextNode(` ${pelicula.titulo}`);
+        titulopelicula.appendChild(rendertitulopelicula);
+        contenedortarjeta.appendChild(titulopelicula);
+
+
+    
+
+
+
+        const redirvermas =document.createElement('a');
+        redirvermas.setAttribute('href',"vermas.js");
+        const btnvermas = document.createElement('button');
+        btnvermas.classList.add('btn-vermas');
+        const renderbtnvermas = document.createTextNode('Ver más...');
+        btnvermas.appendChild(renderbtnvermas);
+        redirvermas.appendChild(btnvermas);
+        contenedortarjeta.appendChild(redirvermas);
+
+        card.appendChild(imagentarjeta);
+        card.appendChild(contenedortarjeta);
+        tarjeta.appendChild(card);
+
+        
+
+        // const descripcionpelicula = document.createElement('p');
+        // const renderdescpelicula = document.createTextNode(`Titulo: ${pelicula.descripcion}`);
+        // descripcionpelicula.appendChild(renderdescpelicula);
+        // contenedortarjeta.appendChild(descripcionpelicula);
+
+
+
+
+
+    });
+
+
+
+
+
+};
+
+rendercards();
